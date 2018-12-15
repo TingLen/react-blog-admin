6d17afd316da3946f6d7cd94cce13dbbe6361531
彭定林 <1979945716@qq.com>
2018-12-15 15:37:55 +0800

    完成分类管理页面表格样式

--- src/page/Home/component/Categories/Categories.js
+++ src/page/Home/component/Categories/Categories.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import { Table,Tag } from 'antd'
 import './Categories.css'
+
+const columns = [{
+    title: "Id",
+    dataIndex: "id",
+    key: "id"
+},{
+    title: "Tag",
+    dataIndex: 'tag',
+    key: "tag"
+},{
+    title: "Action",
+    dataIndex: "action",
+    key: "action",
+    render: () => (
+        <div>
+            <Tag color="#2db7f5">编辑</Tag>
+            <Tag color="red">删除</Tag>
+        </div>
+    )
+}]
+
+const dataSource = [{
+    key: "1",
+    id: "1",
+    tag: "java",
+}]
 
 class Categories extends React.Component {
     render() {
         return (
             <div className="Categories">
-            
+                <Table
+                 columns={columns}
+                 dataSource={dataSource}
+                 pagination={false}/>
             </div>
         )
     }
