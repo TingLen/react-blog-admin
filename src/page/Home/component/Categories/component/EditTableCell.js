4414f99fb411ddb23e5d87bc52d1a1126792f466
彭定林 <1979945716@qq.com>
2018-12-18 01:42:13 +0800

    完善可编辑单元格。

--- src/page/Home/component/Categories/component/EditTableCell.js
+++ src/page/Home/component/Categories/component/EditTableCell.js
@@ -7,7 +7,23 @@ import { Form,Input } from 'antd'
 class EditTableCell extends React.Component {
     constructor(props){
         super(props)
+        
     }
+
+    // componentDidMount(){
+    //     document.addEventListener('click',this.handleClickOutside)
+    // }
+
+    // handleClickOutside = (e) => {
+    //     if(this.cell.input !== e.target){
+    //         this.updateTag(this.props.record)
+    //     }
+    // }
+
+    // updateTag = (record) => {
+    //     console.log(record)
+    // }
+
     render() {
         const {getFieldDecorator} = this.props.form
         return (
@@ -20,7 +36,7 @@ class EditTableCell extends React.Component {
                                 message: `Please Input!`,
                                 }],
                                 initialValue: this.props.record.tag,
-                        })(<Input/>)
+                        })(<Input  ref={node => this.cell = node}/>)
                     }
                 </Form.Item>
             </Form>
